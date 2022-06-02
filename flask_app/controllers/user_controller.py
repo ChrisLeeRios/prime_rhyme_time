ebeeaf5f15ad52e55687d5226e63622e2322a66c
Christopher Rios <chris12k9@yahoo.com>
2022-06-02 13:38:37 -0700

    adding double quotes

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -11,88 +11,88 @@ from flask_app.models.rhyme_model import Rhyme
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return redirect('/login')
+    return redirect("/login")
 
 # I want to make sure a user isnt logged in and if they are then i redirect back to dash board
 # validate email and passwords match and that username has over a minimum length 
 # create the user using the request form and the user method for adding a user
-# add a session['user.id'] to be in there while the person is signed in 
+# add a session["user.id"] to be in there while the person is signed in 
 # store the password using bcrypt
-@app.route('/signup')
+@app.route("/signup")
 def signup():
-    if 'user_id' in session:
-        return redirect('/dashboard')
-    return render_template('signup.html')
+    if "user_id" in session:
+        return redirect("/dashboard")
+    return render_template("signup.html")
 
-@app.route('/signup_post', methods = ['POST'])
+@app.route("/signup_post", methods = ["POST"])
 def signup_post():
     if not User.validate_register(request.form):
-        return redirect('/signup')
-    pw_hash = bcrypt.generate_password_hash(request.form['pw'])
+        return redirect("/signup")
+    pw_hash = bcrypt.generate_password_hash(request.form["pw"])
 
     data = {
-        'username': request.form['username'],
-        'email': request.form['email'],
-        'pw_hash': pw_hash
+        "username": request.form["username"],
+        "email": request.form["email"],
+        "pw_hash": pw_hash
     }
     user_id = User.add_user(data)
-    session['user_id'] = user_id
-    return redirect('/home')
+    session["user_id"] = user_id
+    return redirect("/home")
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    if 'user_id' in session:
-        return redirect('/home')
-    return render_template('login.html')
+    if "user_id" in session:
+        return redirect("/home")
+    return render_template("login.html")
 
-@app.route('/login_post', methods =["POST"])
+@app.route("/login_post", methods =["POST"])
 def login_post():
     data = {
-        'email': request.form['email'],
+        "email": request.form["email"],
     }
     user_in_db = User.get_by_email(data)
     if not user_in_db:
-        flash('Incorrect Email or Password.', 'err.log')
-        return redirect('/login')
-    if not bcrypt.check_password_hash(user_in_db.pw_hash,request.form['pw']):
-        flash('Incorrect Email or Password.', 'err.log')
-        return redirect('/login')
-    session['user_id'] = user_in_db.id
-    return redirect('/home')
+        flash("Incorrect Email or Password.", "err.log")
+        return redirect("/login")
+    if not bcrypt.check_password_hash(user_in_db.pw_hash,request.form["pw"]):
+        flash("Incorrect Email or Password.", "err.log")
+        return redirect("/login")
+    session["user_id"] = user_in_db.id
+    return redirect("/home")
 
 
-@app.route('/home')
+@app.route("/home")
 def home():
-    if 'user_id' not in session:
-        flash('You must register or log in to view content.')
-        return redirect('/')
+    if "user_id" not in session:
+        flash("You must register or log in to view content.")
+        return redirect("/")
     data = {
-        'id': session['user_id']
+        "id": session["user_id"]
     }
     user = User.get_one(data)
     all_users = User.get_all_users()
     rhymes = Rhyme.get_all_rhymes()
-    return render_template('home.html', user = user, rhymes = rhymes, all_users = all_users )
+    return render_template("home.html", user = user, rhymes = rhymes, all_users = all_users )
 
-@app.route('/dashboard')
+@app.route("/dashboard")
 def dashboard():
-    if 'user_id' not in session:
-        flash('You must register or log in to view content.')
-        return redirect('/')
+    if "user_id" not in session:
+        flash("You must register or log in to view content.")
+        return redirect("/")
     data = {
-        'id': session['user_id']
+        "id": session["user_id"]
     }
     user = User.get_one(data)
     all_users = User.get_all_users()
     rhymes = Rhyme.get_all_rhymes()
-    return render_template('dashboard.html', user = user, rhymes = rhymes, all_users = all_users )
+    return render_template("dashboard.html", user = user, rhymes = rhymes, all_users = all_users )
 
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.clear()
-    return redirect('/')
+    return redirect("/")
 
